01c464ac7512442f7b46b10a279b31af09417a82
matheus simoes <matheusaugustosimoes@hotmail.com>
2021-06-25 20:33:51 -0300

    inserção da classe Produto no pacote Comercial

---
+++ src/main/java/ifsuldeminas/bcc/teii/trabalho/estoque/model/entity/comercial/Produto.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.comercial;
+
+/**
+ *
+ * @author matheus
+ */
+
+public class Produto {
+    
+    private int id;
+    private String nome;
+    private int quantidade;
+    private String especificacao;
+
+    public Produto(String nome, int quantidade, String especificacao) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.especificacao = especificacao;
+    }
+
+    public void EditarProduto(int id, String nome, int quantidade, String especificacao) {
+        this.id = id;
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.especificacao = especificacao;
+    }
+    
+    public void DeletarProduto(int id) {
+        //getId(id)     
+    }
+    
+    public void ListarProdutos(){
+        //codigo select * produtos
+    }
+
+    
+    public int getId() {
+        return id;
+        
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getEspecificacao() {
+        return especificacao;
+    }
+
+    public void setEspecificacao(String especificacao) {
+        this.especificacao = especificacao;
+    }
+    
+    
+    
+}
