dced59fdbef8a39fffef21ab04de79b3c062a294
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-06-25 21:37:52 -0300

    Criando classe Nota fiscal

---
+++ src/main/java/ifsuldeminas/bcc/teii/trabalho/estoque/model/entity/comercial/NotaFiscal.java
@@ -0,0 +1,49 @@
+package ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.comercial;
+
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.Funcionario;
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.colaboradores.Colaboradores;
+
+import java.util.Date;
+
+public class NotaFiscal {
+    private int id;
+    private Colaboradores Colaborador;
+    private Date data;
+    private Funcionario funcionario;
+    //private Transacao transacao;
+
+    public NotaFiscal(int id, Colaboradores colaborador, Date data, Funcionario funcionario) {
+        this.id = id;
+        Colaborador = colaborador;
+        this.data = data;
+        this.funcionario = funcionario;
+    }
+
+    public Colaboradores getColaborador() {
+        return Colaborador;
+    }
+
+    public void setColaborador(Colaboradores colaborador) {
+        Colaborador = colaborador;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public void EmitirNotaFiscal(Colaboradores colaborador, Funcionario funcionario /*, Transacao transacao*/){
+        /*TODO: Criar a funõa de emissão da nota fiscal*/
+    }
+}
