dc03d66c2490476b93eede226d69f558dc425619
matheus simoes <matheusaugustosimoes@hotmail.com>
2021-07-19 23:12:13 -0300

    Ajustes no controler de Colaboradores

--- src/main/java/ifsuldeminas/bcc/teii/trabalho/estoque/controller/colaboradores/ColaboradorController.java
+++ src/main/java/ifsuldeminas/bcc/teii/trabalho/estoque/controller/colaboradores/ColaboradorController.java
@@ -1,4 +1,49 @@
 package ifsuldeminas.bcc.teii.trabalho.estoque.controller.colaboradores;
 
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.Funcionario;
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.colaboradores.Colaboradores;
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.repositories.ColaboradorRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/colaboradores")
+@RestController
 public class ColaboradorController {
+    private ColaboradorRepository colaboradorRepository;
+    public ColaboradorController (ColaboradorRepository colaboradorRepository){
+        this.colaboradorRepository = colaboradorRepository;
+    }
+
+    @GetMapping
+    public List<Colaboradores> ListarColaboradores(){
+        return colaboradorRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Colaboradores ListarColaborador(@PathVariable int id){
+        return colaboradorRepository.getById(id);
+    }
+
+    @PostMapping
+    public Colaboradores AdicionarColaborador(@RequestBody Colaboradores colaboradores){
+        return colaboradorRepository.save(colaboradores);
+    }
+
+    @PutMapping("/{id}")
+    public Colaboradores AtualizarColaborador(@PathVariable int id, @RequestBody Colaboradores colaboradores){
+        Colaboradores aux = colaboradorRepository.getById(id);
+        aux.setNome(colaboradores.getNome());
+        aux.setEndereco(colaboradores.getEndereco());
+        aux.setTelefone(colaboradores.getTelefone());
+      //tipo não atualizavel
+        return colaboradorRepository.save(aux);
+    }
+
+    @DeleteMapping("/{id}")
+    public void DeletarColaborador(@PathVariable int id){
+        colaboradorRepository.deleteById(id);
+    }
+
 }
