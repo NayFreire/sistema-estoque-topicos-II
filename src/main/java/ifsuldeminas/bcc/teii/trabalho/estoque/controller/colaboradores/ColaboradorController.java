3940d41b385fc1e38ac245b9237fd3bf187f4240
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-07-19 23:15:24 -0300

    Conrtroller de Colaboradores

--- src/main/java/ifsuldeminas/bcc/teii/trabalho/estoque/controller/colaboradores/ColaboradorController.java
+++ src/main/java/ifsuldeminas/bcc/teii/trabalho/estoque/controller/colaboradores/ColaboradorController.java
@@ -1,4 +1,28 @@
 package ifsuldeminas.bcc.teii.trabalho.estoque.controller.colaboradores;
 
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.colaboradores.Colaboradores;
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.repositories.ColaboradorRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/colaboradores")
+@RestController
 public class ColaboradorController {
+    private ColaboradorRepository colaboradorRepository;
+
+    public ColaboradorController(ColaboradorRepository colaboradorRepository){
+        this.colaboradorRepository = colaboradorRepository;
+    }
+
+    @GetMapping
+    public List<Colaboradores> ListarColaboradores(){
+        return colaboradorRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Colaboradores MostrarColaborador(@PathVariable int id){
+        return colaboradorRepository.getById(id);
+    }
+
 }
