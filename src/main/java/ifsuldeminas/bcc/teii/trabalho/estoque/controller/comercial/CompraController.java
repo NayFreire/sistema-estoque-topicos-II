7012972329425f6e65c5ce37c40af824cba6fba1
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-07-25 19:25:58 -0300

    Adicionando a requisição post em compras

--- src/main/java/ifsuldeminas/bcc/teii/trabalho/estoque/controller/comercial/CompraController.java
+++ src/main/java/ifsuldeminas/bcc/teii/trabalho/estoque/controller/comercial/CompraController.java
@@ -1,8 +1,12 @@
 package ifsuldeminas.bcc.teii.trabalho.estoque.controller.comercial;
 
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.colaboradores.Cliente;
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.colaboradores.Fornecedor;
 import ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.comercial.Compra;
 import ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.comercial.Produto;
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.entity.comercial.Venda;
 import ifsuldeminas.bcc.teii.trabalho.estoque.model.repositories.CompraRepository;
+import ifsuldeminas.bcc.teii.trabalho.estoque.model.repositories.FornecedorRepository;
 import ifsuldeminas.bcc.teii.trabalho.estoque.model.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +22,12 @@ public class CompraController {
     @Autowired
     private CompraRepository compraRepository;
     private ProdutoRepository produtoRepository;
+    private FornecedorRepository fornecedorRepository;
 
-    public CompraController(CompraRepository compraRepository, ProdutoRepository produtoRepository) {
+    public CompraController(CompraRepository compraRepository, ProdutoRepository produtoRepository, FornecedorRepository fornecedorRepository) {
         this.compraRepository = compraRepository;
         this.produtoRepository = produtoRepository;
+        this.fornecedorRepository = fornecedorRepository;
     }
 
     @GetMapping
@@ -34,12 +40,19 @@ public class CompraController {
         return compraRepository.getById(id);
     }
 
-    @PostMapping("/{idProduto}")
-    public Compra AdicionarCompra(@RequestBody Compra compra, @PathVariable int idProduto){
+    @PostMapping("/{idProduto}/fornecedor/{idFornecedor}")
+    public Compra AdicionarVenda(@RequestBody Compra compra, @PathVariable int idProduto, @PathVariable int idFornecedor){
         Optional<Produto> getProduto = produtoRepository.findById(idProduto);
         Produto produto = getProduto.get();
+
+        Optional<Fornecedor> getFornecedor = fornecedorRepository.findById(idFornecedor);
+        Fornecedor fornecedor = getFornecedor.get();
+
+        compra.setFornecedor(fornecedor);
+
         Set<Produto> produtos = new LinkedHashSet<>();
         produtos.add(produto);
+
         compra.setProdutos(produtos);
 
         return compraRepository.save(compra);
